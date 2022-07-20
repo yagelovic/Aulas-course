83c956162769c50dd302b7e6056c98b213053bb6
Paulo <paulojose.yagelovic@gmail.com>
2022-07-20 16:10:43 -0300

    segunda parte da primeira aula de javascript

--- 01-fundamentos/04-introducao-javascript/04-introducao-javascript/scripts.js
+++ 01-fundamentos/04-introducao-javascript/04-introducao-javascript/scripts.js
@@ -1,14 +1,38 @@
-let age = 20;
-let name = "Hulk";
+// let age = 20;
+// let nome = "Hulk";
 
-console.log(age);
-console.log(name);
+// console.log(age);
+// console.log(nome);
 
-const myName = "Paulo José";
-const birthCity = "Brasília";
-let birthYear = 1990;
-birthYear = 2030;
+// const myName = "Paulo José";
+// const birthCity = "Brasília";
+// let birthYear = 1990;
+// birthYear = 2030;
 
-console.log(myName);
-console.log(birthCity);
-console.log(birthYear);
+// console.log(myName);
+// console.log(birthCity);
+// console.log(birthYear);
+
+// let movie = "avengers"; //string literal (é uma string)
+// let score = 10; //number literal (valor numerico)
+// let isValid = true; //boolean
+// let name; //undefined (indefinida)
+// let color = null; //espera redefinição de valor
+
+
+
+// //+. -, *, /, **
+
+ let salary = 3500;
+
+// console.log(salary + salary);
+// console.log(salary - salary);
+// console.log(salary * salary);
+// console.log(salary / salary);
+// console.log(3 ** 3);
+
+salary++;
+
+console.log(salary);
+
+
