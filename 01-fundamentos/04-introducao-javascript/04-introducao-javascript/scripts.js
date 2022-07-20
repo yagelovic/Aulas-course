6bdc6a19ec56791b983ccc5dbfc08ea2e100787f
Paulo <paulojose.yagelovic@gmail.com>
2022-07-20 15:53:53 -0300

    introdução ao javaScript

---
+++ 01-fundamentos/04-introducao-javascript/04-introducao-javascript/scripts.js
@@ -0,0 +1,14 @@
+let age = 20;
+let name = "Hulk";
+
+console.log(age);
+console.log(name);
+
+const myName = "Paulo José";
+const birthCity = "Brasília";
+let birthYear = 1990;
+birthYear = 2030;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
